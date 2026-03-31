8bbbd5cfc6966580815865b3218de132063c3e94
LuciuZx1PAC <107567899+ezio1123@users.noreply.github.com>
2026-03-31 07:59:44 +0700

    Update OWNER_ID and SUPPORT_CHAT values
    Signed-off-by: LuciuZx1PAC  <107567899+ezio1123@users.noreply.github.com>

--- config.py
+++ config.py
@@ -6,5 +6,5 @@ load_dotenv()
 BOT_TOKEN = getenv("BOT_TOKEN")
 MONGO_URL = getenv("MONGO_URL")
 
-OWNER_ID = int(getenv("OWNER_ID", 1356469075))
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/FallenAssociation")
+OWNER_ID = int(getenv("OWNER_ID", 5707436791))
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/satanR10")
