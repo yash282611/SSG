ed57a1cfac6446b84f0de3ca226ac7fe9ef9424b
yash282611 <yash827830@gmail.com>
2026-08-09 19:22:40 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ load_dotenv()
 
 BOT_TOKEN = getenv("BOT_TOKEN", "8867636200:AAHC0dWdhWrkVUWzg0eUALsW0eTdsQRUB48")
 
-MONGO_URL = getenv("mongodb+srv://yash827830_db_user:DyLsRtr2W1iLL8SK@yash.yprhxeh.mongodb.net/?retryWrites=true&w=majority")
+MONGO_URL = getenv("MONGO_URL","mongodb+srv://yash827830_db_user:DyLsRtr2W1iLL8SK@yash.yprhxeh.mongodb.net/?retryWrites=true&w=majority")
 
 OWNER_ID = int(getenv("OWNER_ID", 8203857803))
 SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/+b9RbWnTQ4PFiNjZl")
