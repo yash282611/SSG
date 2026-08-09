d9f8164725bd0b068a98a355401ec9e9e89033e0
yash282611 <yash827830@gmail.com>
2026-08-09 18:57:24 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,6 +4,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 BOT_TOKEN = getenv("8867636200:AAGZbbyHgH0oUsjkidG6Ed-5BlW3AqjcynA")
+
 MONGO_URL = getenv("mongodb+srv://yash827830_db_user:DyLsRtr2W1iLL8SK@yash.yprhxeh.mongodb.net/?retryWrites=true&w=majority")
 
 OWNER_ID = int(getenv("OWNER_ID", 8203857803))
